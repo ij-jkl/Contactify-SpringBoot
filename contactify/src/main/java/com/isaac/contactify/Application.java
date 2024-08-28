db34a1ea1039d5cede2ecf04be1460e628eaed29
Isaac <isaaz.jordan02@gmail.com>
2024-08-28 18:25:24 -0300

    Dependencies loaded

--- contactify/src/main/java/com/isaac/contactify/Application.java
+++ contactify/src/main/java/com/isaac/contactify/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactifyApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContactifyApplication.class, args);
+		SpringApplication.run(com.isaac.contactify.Application.class, args);
 	}
 
 }
