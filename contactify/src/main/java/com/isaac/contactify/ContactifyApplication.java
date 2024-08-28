352d49b2f6d4d73b3b418a54c9fec86315efb82b
Isaac <isaaz.jordan02@gmail.com>
2024-08-28 17:16:31 -0300

    Initial Commit, New Repository

---
+++ contactify/src/main/java/com/isaac/contactify/ContactifyApplication.java
@@ -0,0 +1,13 @@
+package com.isaac.contactify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactifyApplication.class, args);
+	}
+
+}
---
+++ contactify/src/test/java/com/isaac/contactify/ContactifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaac.contactify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
