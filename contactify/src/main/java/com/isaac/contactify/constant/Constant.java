84f975d4d8136942b5bc4d2d883160a20bba64a3
Isaac <isaaz.jordan02@gmail.com>
2024-09-02 18:01:52 -0300

    fix: Path to save IMG Fixed and Tested with Postman

--- contactify/src/main/java/com/isaac/contactify/constant/Constant.java
+++ contactify/src/main/java/com/isaac/contactify/constant/Constant.java
@@ -2,5 +2,5 @@ package com.isaac.contactify.constant;
 
 public class Constant {
     // The user.home it's going to reference the Users name, so it's a good way to make sure that it's going to work on anyone's PC
-    public static final String PHOTO_DIRECTORY = System.getProperty( "user.home") + "Donwoloads/uploads";
+    public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
 }
